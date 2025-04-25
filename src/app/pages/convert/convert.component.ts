a2970d25bdd4a1ed00532b73c734130219fb8a59
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-04-25 13:36:13 +0200

    första utkast på konvertera värden

--- src/app/pages/convert/convert.component.ts
+++ src/app/pages/convert/convert.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.css'
 })
 export class ConvertComponent {
+  meters: number = 0;
+  feet: number = 0;
+  celsius: number = 0;
+  fahrenheit: number = 0;
 
+  convertToFeet() {
+    this.feet = this.meters * 3.28084;
+  }
+
+  convertToMeters() {
+    this.meters = this.feet / 3.28084;
+  }
+
+  convertToFahrenheit() {
+    this.fahrenheit = (this.celsius * 9 / 5) + 32;
+  }
+
+  convertToCelsius() {
+    this.celsius = (this.fahrenheit - 32) * 5 / 9;
+  }
 }
