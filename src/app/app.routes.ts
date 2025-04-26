1e37f4abe1370a51e455259f30bc318c92e5f81e
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-04-27 00:26:25 +0200

    om inget anges, är startsidan alltid startsida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ConvertComponent } from './pages/convert/convert.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: "home", component: HomeComponent},
     {path: "about", component: AboutComponent},
     {path: "convert", component: ConvertComponent}
